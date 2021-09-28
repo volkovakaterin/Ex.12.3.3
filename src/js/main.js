eb5d48e04507cae6eeaa8e17da34c76af88074ae
Ekaterina Volkova <volkovakaterin@gmail.com>
2021-09-28 17:37:38 +0400

    initil commit

--- src/js/main.js
+++ src/js/main.js
@@ -11,34 +11,50 @@ const cursor = new Cursor();
 
 class Game {
   constructor() {
-    this.missedMove = 0;
-    this.point = 0;
+    this.missedMove = Number(document.querySelector('.missed-move').textContent);
+    this.point = Number(document.querySelector('.point').textContent);
     this.area = document.querySelector('#area');
+    this.timer = 0;
   }
 
   init() {
-    document.addEventListener('DOMContentLoaded', () => {
-      setInterval(() => {
-        render.renderer();
-        this.missedMove += 1;
-        console.log(this.missedMove);
-        this.gameOver();
-      }, 1000);
-    });
+    // document.addEventListener('DOMContentLoaded', () => {
+    this.timer = setInterval(() => {
+      render.renderer();
+      this.missedMove += 1;
+      document.querySelector('.missed-move').textContent = this.missedMove;
+      console.log(this.missedMove);
+      this.gameOver();
+    }, 1000);
+    // });
     this.area.addEventListener('mousedown', (event) => {
       const { target } = event;
       if (target.classList.contains('box_with_goblin')) {
         cursor.changeCursor(event);
         this.missedMove -= 1;
         this.point += 1;
+        document.querySelector('.point').textContent = this.point;
       }
+    });
+    document.querySelector('.new-game').addEventListener('click', () => {
+      this.newGame();
     });
   }
 
   gameOver() {
     if (this.missedMove === 5) {
       alert('Игра окончена!');
+      clearInterval(this.timer);
     }
+  }
+
+  newGame() {
+    clearInterval(this.timer);
+    document.querySelector('.missed-move').textContent = 0;
+    document.querySelector('.point').textContent = 0;
+    this.missedMove = 0;
+    this.point = 0;
+    this.init();
   }
 }
 
